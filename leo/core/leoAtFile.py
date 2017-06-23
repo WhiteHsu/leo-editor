aa6adbfcae3a3a7079ce9d9fe58376f66bac7b98
Edward K. Ream <edreamleo@gmail.com>
2017-06-23 09:01:30 -0500

    Getting Things Done
    Leo build: 20170623090130

--- leo/core/leoAtFile.py
+++ leo/core/leoAtFile.py
@@ -359,7 +359,7 @@ class AtFile(object):
     #@+node:ekr.20100619222623.5918: *4* << Detecting clone conflicts >>
     #@+at
     # 
-    # Changed in read-test 3.
+    # Changed in read-test 4.
     # 
     # **v.tempRoots**, a *temp* ivar, contains root @file nodes.
     # 
